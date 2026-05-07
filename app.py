108c31009105c2654f9d519216ad367ec7e0dbc5
sddoAlotibi79 <>
2026-05-08 00:10:42 +0300

    fix render port

--- app.py
+++ app.py
@@ -160,8 +160,5 @@ def logout():
 print("STARTING FLASK APP...")
 
 
-app.run(
-    host="127.0.0.1",
-    port=5000,
-    debug=True
-)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
